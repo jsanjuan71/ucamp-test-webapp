a9d9c0f41b352b446618e1a44336b786dc0c84bf
Julio Sanjuan <julio.sanjuan@konfio.mx>
2022-11-03 08:27:57 -0600

    feature: Searchbox interaction

--- ucamp-app/src/App.js
+++ ucamp-app/src/App.js
@@ -1,33 +1,42 @@
-import React ,{useState, useEffect} from 'react';
+import React ,{useState, useEffect, } from 'react';
+import M from "materialize-css";
 import './App.css';
 import ProductCard from './components/ProductCard/ProductCard';
 import ProductService from './services/ProductService';
 
 function App() {
   const [products, setProducts] = useState( [] );
-  
-  useEffect(() => {
-    async function fetchData() {
-      const response = await ProductService.search("iphone");
+  const [seachText, setSeachText] = useState( "" );
+
+
+  async function handleChangeSearch({target}) {
+      const response = await ProductService.search(seachText);
       setProducts(response);
-    }
-    fetchData();
-  }, []); 
+      setSeachText("");
+  }
 
   return (
     <>
       <nav>
         <div class="nav-wrapper">
-          <form>
+          <form onSubmit={handleChangeSearch}>
             <div class="input-field">
-              <input id="search" type="search" required placeholder="Escriba y presione enter" />
+              <input onInput={({target}) => setSeachText(target.value)} value={seachText} id="search" type="search" required placeholder="Escriba y presione enter" />
               <label class="label-icon" for="search"><i class="material-icons">search</i></label>
               <i class="material-icons">close</i>
             </div>
           </form>
         </div>
       </nav>
-      <h6>Products</h6>
+      <nav>
+        <div class="nav-wrapper">
+          <a href="#" class="brand-logo">Catálogo</a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            
+          </ul>
+        </div>
+      </nav>
+
       <div class="row">
         { products.length &&
           products.map((prod) => {
@@ -44,7 +53,7 @@ function App() {
             )
           })
         }
-      </div>  
+      </div>
     </>
   );
 }
