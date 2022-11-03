5456c66e31f033eb9c6e51814c2e225f57eacfba
Julio Sanjuan <julio.sanjuan@konfio.mx>
2022-11-03 09:03:04 -0600

    Feature: added Selector components

--- ucamp-app/src/App.js
+++ ucamp-app/src/App.js
@@ -1,18 +1,42 @@
-import React ,{useState, useEffect, } from 'react';
+import React ,{useState, useEffect } from 'react';
 import M from "materialize-css";
 import './App.css';
 import ProductCard from './components/ProductCard/ProductCard';
+import Selector from './components/Selector/Selector';
+
 import ProductService from './services/ProductService';
 
 function App() {
   const [products, setProducts] = useState( [] );
+  const [sortEnabled, setSortEnabled] = useState( false );
   const [seachText, setSeachText] = useState( "" );
+  const [conditions, setConditions] = useState( [{"value":"new", "label":"new"}] );
+  
+  const priceLevels = [
+    {value: "menor", label: "Menor precio"},
+    {value: "mayor", label: "Mayor precio"}
+  ];
 
+  useEffect( () => {
+    M.AutoInit();
+  }, []);
 
   async function handleChangeSearch({target}) {
       const response = await ProductService.search(seachText);
       setProducts(response);
       setSeachText("");
+      if(response.length > 0) {
+        setSortEnabled(true);
+        M.AutoInit();
+      }
+  }
+
+  async function handleSelectedCondition(selected) {
+    console.log("condition:", selected);
+  }
+
+  async function handleSelectedPriceLevel(selected) {
+    console.log("price:", selected);
   }
 
   return (
@@ -36,6 +60,21 @@ function App() {
           </ul>
         </div>
       </nav>
+      <div class="row">
+        <Selector
+          disabled={!sortEnabled}
+          options={priceLevels}
+          title="Ordenar por precio"
+          onSelect={handleSelectedPriceLevel}
+         />
+
+        <Selector
+          disabled={!sortEnabled}
+          options={conditions}
+          title="Ordenar por condición"
+          onSelect={handleSelectedCondition}
+         />
+      </div>
 
       <div class="row">
         { products.length &&
